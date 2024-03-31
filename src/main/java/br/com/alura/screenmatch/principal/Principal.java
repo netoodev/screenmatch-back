0b3f8123c3828fbdd549f05879d50b001e1aa65e
Neto <netoodeveloper@gmail.com>
2024-03-31 16:21:58 -0300

    Listando séries buscadas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -19,36 +19,45 @@ public class Principal {
 
     private final String ENDERECO = "https://omdbapi.com/?t=";
     private final String API_KEY = "&apikey=d11206a3";
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     public void exibeMenu() {
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                                
-                0 - Sair
-                """;
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
+                                    
+                    0 - Sair
+                    """;
 
-        System.out.println(menu);
-        var opcao = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
     }
 
         private void buscarSerieWeb() {
             DadosSerie dados = getDadosSerie();
+            dadosSeries.add(dados);
             System.out.println(dados);
         }
 
@@ -72,5 +81,9 @@ public class Principal {
                 temporadas.add(dadosTemporada);
             }
             temporadas.forEach(System.out::println);
+        }
+
+        private void listarSeriesBuscadas() {
+            dadosSeries.forEach(System.out::println);
         }
 }
