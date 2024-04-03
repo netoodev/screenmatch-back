c2902f589dcf23e1860e1aadbad3787f3c01730e
Neto <netoodeveloper@gmail.com>
2024-04-02 23:47:28 -0300

    Retornando os detalhes da série

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,5 +32,16 @@ public class SerieService {
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(),
                         s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorID(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(),
+                    s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
